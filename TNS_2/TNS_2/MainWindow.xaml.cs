97871bfcffb7660c7f9cfda57c232b7c42e81db0
aleksandrkanaikin <aleksandrkanajkin7@gmail.com>
2023-05-20 02:20:06 +0300

    Session 2 work

---
+++ TNS_2/TNS_2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TNS_2.Pages;
+
+namespace TNS_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Clients_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new SubscribersPage());
+        }
+
+        private void EquipmentControl_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Assets_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Billing_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Support_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CRMPage());
+        }
+    }
+}
---
+++ TNS_2/TNS_2/Pages/SubscribersPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TNS_2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для SubscribersPage.xaml
+    /// </summary>
+    public partial class SubscribersPage : Page
+    {
+
+        TeleconNevaEntities db = new TeleconNevaEntities();
+        public SubscribersPage()
+        {
+            InitializeComponent();
+
+            Clients_Grid.ItemsSource = db.Subs.ToList();
+        }
+    }
+}
